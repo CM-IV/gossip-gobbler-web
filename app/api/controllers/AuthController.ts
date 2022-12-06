4c388bd35132ec8ad9095516f998588a85b3fa05
CM-IV <cmathIV@protonmail.com>
2022-12-06 16:33:10 -0600

    Moved some deps to devdeps, added flashmessage for confirm password

--- app/api/controllers/AuthController.ts
+++ app/api/controllers/AuthController.ts
@@ -21,7 +21,18 @@ export default class AuthController {
       ]),
     })
 
-    const { email, username, password } = await request.validate({ schema: userSchema })
+    const { email, username, password } = await request.validate({ 
+      schema: userSchema,
+      messages: {
+        required: "The {{ field }} is required to register a new account.",
+        confirmed: "The password fields do not match.",
+        "username.unique": "That username is already in use",
+        "email.email": "Enter a valid email address",
+        "email.unique": "That email is already in use",
+        "password.minLength":
+          "The minimum characters in your password must be greater than or equal to 5.",
+      },
+    })
     const user = await User.create({ email, username, password })
     await user.related('profile').create({ name: username })
 
